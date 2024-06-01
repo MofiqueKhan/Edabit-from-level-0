af95866f4d5c499eefb45423ed6e1debaa95f112
MofiqueKhan <khanmofique@gmail.com>
2024-06-01 19:38:37 +0530

    doing array's questions

--- day5.js
+++ day5.js
@@ -102,9 +102,9 @@ function filterArray(arr) {
 // In mathematics and digital electronics, a binary number is a number expressed in the base-2 numeral system or binary numeral system. Given an array of ones and zeroes of a binary number, return the equivalent decimal value.
 
 // Examples
-function binaryToDecimal(arr){
-    let result = arr.join("")
-    return parseInt(result , 2)
+function binaryToDecimal(arr) {
+  let result = arr.join("");
+  return parseInt(result, 2);
 }
 // console.log( binaryToDecimal([0, 0, 0, 1]) , 1);
 
@@ -118,15 +118,114 @@ function binaryToDecimal(arr){
 // Given an object, return a string that concatenates all the values and adds the 2nd key at the end. Make sure you keep an empty space between them but not at the beginning or end of the string. Look at the examples for a clearer picture.
 
 // Examples
-let obj1 = { 1: "Mommy", 2: "please", 3: "help" } // "Mommy please help please"
-let obj2 = { 1: "Me", 2: "innocent", 3: "is" } // "Me innocent is innocent"
-let obj3 = { 1: "Must", 2: "lawyer", 3: "call" } // "Must lawyer call lawyer"
-function fn(obj){
-    return Object.values(obj).join(" ") + " " + Object.values(obj)[1] 
+let obj1 = { 1: "Mommy", 2: "please", 3: "help" }; // "Mommy please help please"
+let obj2 = { 1: "Me", 2: "innocent", 3: "is" }; // "Me innocent is innocent"
+let obj3 = { 1: "Must", 2: "lawyer", 3: "call" }; // "Must lawyer call lawyer"
+function fn(obj) {
+  return Object.values(obj).join(" ") + " " + Object.values(obj)[1];
 }
 // console.log(fn(obj1));
 // console.log(fn(obj2));
 // console.log(fn(obj3));
 
+// 48.
+// Create a function that returns an array of booleans from a given number by iterating through the number one digit at a time and appending true into the array if the digit is 1 and false otherwise.
 
+// Examples
+function integerBoolean(str) {
+  return str.split("").map((e) => e != false);
+}
+// console.log( integerBoolean("100101") , [true, false, false, true, false, true]);
+
+// console.log( integerBoolean("10") , [true, false]);
+
+// console.log( integerBoolean("001") , [false, false, true]);
+
+// 49.
+// According to the lodash documentation, _.fill Fills elements of an array with the value from start to, but not including, end. Note that this method mutates the array.
+
+// This challenge requires you to write your own version of this function without using lodash so that you can better understand it works.
+
+// Arguments
+// array (Array): The array to fill.
+// value (*): The value to fill array with.
+// [start=0] (number): The start position.
+// [end=array.length] (number): The end position.
+// Returns
+// (Array): Returns array.
+// Examples
+var array = [1, 2, 3];
+
+function fill(array, value, start = 0, end = array.length) {
+  // Ensure start is at least 0
+  start = Math.max(start, 0);
+  console.log(start);
+  // Ensure end does not exceed array length
+  end = Math.min(end, array.length);
+  console.log(end);
+
+  // Fill the array from start to end (not including end)
+  for (let i = start; i < end; i++) {
+    array[i] = value;
+  }
+
+  // Return the modified array
+  return array;
+}
+// console.log(fill(array, "a"), ["a", "a", "a"]);
+
+// console.log(fill(Array(3), 2), [2, 2, 2]);
+
+// console.log(fill([4, 6, 8, 10], "*", 1, 3), [4, "*", "*", 10]);
+
+// 50.
+// Write a function that accepts the width and height (m, n) and an optional proc s and generates an array with m elements. Each element is a string consisting of either:
+
+// The default character (hash #) repeating n times (if no proc is given).
+// The character passed in through the proc repeating n times.
+// Examples
+function makeRug(num1, num2, symbol = "#") {
+  // return new Array(m).fill(symbol.repeat(n));
+
+  let result = [];
+  for (let i = 0; i < num1; i++) {
+    result.push(symbol.repeat(num2));
+  }
+  return result;
+}
+
+// console.log(makeRug(3, 5), ["#####", "#####", "#####"]);
+
+// console.log(makeRug(3, 5, "$"), ["$$$$$", "$$$$$", "$$$$$"]);
+
+// console.log(makeRug(2, 2, "A"), ["AA", "AA"]);
+
+// 51.
+// Create a function that takes a year as an argument and returns the corresponding Chinese zodiac.
+
+// Examples
+function chineseZodiac(birthyear) {
+  const zodiac = [
+    "Rat",
+    "Ox",
+    "Tiger",
+    "Rabbit",
+    "Dragon",
+    "Snake",
+    "Horse",
+    "Goat",
+    "Monkey",
+    "Rooster",
+    "Dog",
+    "Pig",
+  ];
+  const index = (birthyear - 1924) % 12;
+
+  return zodiac[index];
+}
+console.log(chineseZodiac(2021), "Ox");
+
+console.log(chineseZodiac(2020), "Rat");
+
+console.log(chineseZodiac(1933), "Rooster");
 
